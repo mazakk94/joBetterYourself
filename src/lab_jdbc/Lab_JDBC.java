7f3aeeffc36f2f95c7e02f4e8f997362b6fa84a9
mazakk94 <mazakk94@gmail.com>
2015-11-17 22:28:14 +0100

    edit

--- src/lab_jdbc/Lab_JDBC.java
+++ src/lab_jdbc/Lab_JDBC.java
@@ -1,8 +1,28 @@
 package lab_jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Lab_JDBC {
 
     public static void main(String[] args) {
         
+        Connection conn = null;
+        Properties connectionProps = new Properties();
+        connectionProps.put("user", "login");
+        connectionProps.put("password", "haslo");
+        try {
+        conn = DriverManager.getConnection
+        ("jdbc:oracle:thin:@//admlab2-main.cs.put.poznan.pl:1521/dblab01.cs.put.poznan.pl", connectionProps);
+        System.out.println("Połączono z bazą danych");
+        } catch (SQLException ex) {
+        Logger.getLogger(Lab_JDBC.class.getName()).log(Level.SEVERE,
+        "nie udało się połączyć z bazą danych", ex);
+        System.exit(-1);
+        }
     }
 }
