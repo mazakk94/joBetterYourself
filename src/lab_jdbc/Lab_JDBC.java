b1741dec3ddb0fdb47885df81e8821566efbc363
mazakk94 <mazakk94@gmail.com>
2015-12-09 19:58:51 +0100

    gui seems to work ok, let's refactor code to Connector

--- src/lab_jdbc/Lab_JDBC.java
+++ src/lab_jdbc/Lab_JDBC.java
@@ -1,5 +1,7 @@
 package lab_jdbc;
 
+import java.awt.Color;
+//import java.awt.FlowLayout;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -9,9 +11,37 @@ import java.sql.Statement;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Lab_JDBC extends JFrame {
+
+    private JFrame frame;
+    private JPanel panel;
+    private JButton button;
+    private JLabel label;
+
+    public Lab_JDBC(String title) {
+
+        //super(title);                     // invoke the JFrame constructor
+        frame = new JFrame(title);
+        frame.setVisible(true);
+        frame.setSize(600, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //setLayout(new FlowLayout());      // set the layout manager
+
+        panel = new JPanel();
+        panel.setBackground(Color.red);
+
+        button = new JButton("Click Me!"); // construct a JButton
+        label = new JLabel("Label1");
+
+        panel.add(button);
+        panel.add(label);
+        frame.add(panel);
+    }
 
     final public static void printResultSet(ResultSet rs) throws SQLException {
         ResultSetMetaData rsmd = rs.getMetaData();
@@ -52,7 +82,10 @@ public class Lab_JDBC extends JFrame {
 
     }
 
+    @SuppressWarnings("null")
     public static void main(String[] args) {
+
+        new Lab_JDBC("Koksy");
 
         Connection conn = null;
         Properties connectionProps = new Properties();
@@ -89,27 +122,27 @@ public class Lab_JDBC extends JFrame {
         } catch (SQLException ex) {
             Logger.getLogger(Lab_JDBC.class.getName()).log(Level.SEVERE, null, ex);
         }
-/*
-        try {
-            rs.close();
-            System.out.println("Rozłączono ResultSet");
-        } catch (SQLException ex) {
-            Logger.getLogger(Lab_JDBC.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        /*
+         try {
+         rs.close();
+         System.out.println("Rozłączono ResultSet");
+         } catch (SQLException ex) {
+         Logger.getLogger(Lab_JDBC.class.getName()).log(Level.SEVERE, null, ex);
+         }
 
-        try {
-            rs = stmt.executeQuery("SELECT NAZWISKO FROM PRACOWNICY");
-            System.out.println("Udało się stworzyć ResultSet");
-        } catch (SQLException ex) {
-            Logger.getLogger(Lab_JDBC.class.getName()).log(Level.SEVERE, null, ex);
-        }
+         try {
+         rs = stmt.executeQuery("SELECT NAZWISKO FROM PRACOWNICY");
+         System.out.println("Udało się stworzyć ResultSet");
+         } catch (SQLException ex) {
+         Logger.getLogger(Lab_JDBC.class.getName()).log(Level.SEVERE, null, ex);
+         }
 
-        try {
-            printResultSet(rs);
-        } catch (SQLException ex) {
-            Logger.getLogger(Lab_JDBC.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        */
+         try {
+         printResultSet(rs);
+         } catch (SQLException ex) {
+         Logger.getLogger(Lab_JDBC.class.getName()).log(Level.SEVERE, null, ex);
+         }
+         */
         //TU NIE TRZEBA ZAMYKAC RS BO JEST ZAMYKANY W CLOSEEVERYTHING
         closeEverything(rs, stmt, conn);
 
