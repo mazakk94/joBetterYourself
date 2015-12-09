825beb56580c34af50e34a53d8db5afd07fdbfff
Maciej Wojtasik <mazakk94@gmail.com>
2015-12-09 12:28:28 +0100

    connected to DB!

--- src/lab_jdbc/Lab_JDBC.java
+++ src/lab_jdbc/Lab_JDBC.java
@@ -2,34 +2,49 @@ package lab_jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.sql.Statement;
 import java.util.Properties;
-
-
-
-
-
-
-
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Lab_JDBC {
 
     public static void main(String[] args) {
-        
+
         Connection conn = null;
         Properties connectionProps = new Properties();
-        connectionProps.put("user", "login");
-        connectionProps.put("password", "haslo");
+        connectionProps.put("user", "inf117192");
+        connectionProps.put("password", "inf117192");
+
         try {
-            conn = DriverManager.getConnection
-            ("jdbc:oracle:thin:@//admlab2-main.cs.put.poznan.pl:1521/dblab01.cs.put.poznan.pl", connectionProps);
+            conn = DriverManager.getConnection("jdbc:oracle:thin:@//admlab2-main.cs.put.poznan.pl:1521/dblab01.cs.put.poznan.pl", connectionProps);
             System.out.println("Połączono z bazą danych");
         } catch (SQLException ex) {
             Logger.getLogger(Lab_JDBC.class.getName()).log(Level.SEVERE, "nie udało się połączyć z bazą danych", ex);
             System.exit(-1);
+        }
+
+        Statement stmt = null;
+        try {
+            stmt = conn.createStatement();
+        } catch (SQLException ex) {
+            Logger.getLogger(Lab_JDBC.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        ResultSet rs;
+        try {
+            rs = stmt.executeQuery("SELECT * FROM PRACOWNICY");
+        } catch (SQLException ex) {
+            Logger.getLogger(Lab_JDBC.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        try {
+            conn.close();
+            System.out.println("Rozłączono z bazą danych");
+        } catch (SQLException ex) {
+            Logger.getLogger(Lab_JDBC.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
