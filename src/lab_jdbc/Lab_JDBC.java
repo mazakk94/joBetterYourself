3548709fdb265cdec0b490ace3e76aed7d85b4a6
Maciej Wojtasik <mazakk94@gmail.com>
2015-12-09 12:35:04 +0100

    print DB in console

--- src/lab_jdbc/Lab_JDBC.java
+++ src/lab_jdbc/Lab_JDBC.java
@@ -3,6 +3,7 @@ package lab_jdbc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
@@ -10,6 +11,20 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Lab_JDBC {
+
+    final public static void printResultSet(ResultSet rs) throws SQLException {
+        ResultSetMetaData rsmd = rs.getMetaData();
+        int columnsNumber = rsmd.getColumnCount();
+        while (rs.next()) {
+            for (int i = 1; i <= columnsNumber; i++) {
+                if (i > 1) {
+                    System.out.print(" | ");
+                }
+                System.out.print(rs.getString(i));
+            }
+            System.out.println("");
+        }
+    }
 
     public static void main(String[] args) {
 
@@ -33,9 +48,22 @@ public class Lab_JDBC {
             Logger.getLogger(Lab_JDBC.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-        ResultSet rs;
+        ResultSet rs = null;
         try {
             rs = stmt.executeQuery("SELECT * FROM PRACOWNICY");
+        } catch (SQLException ex) {
+            Logger.getLogger(Lab_JDBC.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        try {
+            boolean w = rs.next();
+            //System.out.println("w -> " + w);
+        } catch (SQLException ex) {
+            Logger.getLogger(Lab_JDBC.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        try {
+            printResultSet(rs);
         } catch (SQLException ex) {
             Logger.getLogger(Lab_JDBC.class.getName()).log(Level.SEVERE, null, ex);
         }
