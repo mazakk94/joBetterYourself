d44029c383a424ea4c375d6cb8141525394a9524
mazakk94 <mazakk94@gmail.com>
2015-12-16 12:42:41 +0100

    update

--- src/lab_jdbc/FrameMaker.java
+++ src/lab_jdbc/FrameMaker.java
@@ -1,5 +1,6 @@
 package lab_jdbc;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import javax.swing.*;
 
@@ -11,20 +12,26 @@ public class FrameMaker extends JFrame {
     private JLabel label;
 
     public FrameMaker(String title) {
-
+        
+        GuiInit(title);
+    }
+    
+    private void GuiInit(String title){
+        
         frame = new JFrame(title);
         frame.setVisible(true);
         frame.setSize(1024, 512);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         panel = new JPanel();
-        panel.setBackground(Color.blue);
+        panel.setSize(512, 256);
+        panel.setBackground(Color.gray);
 
         button = new JButton("Click Me!");
         label = new JLabel("Label1");
 
         panel.add(button);
         panel.add(label);
-        frame.add(panel);
+        frame.add(panel, BorderLayout.CENTER);
     }
 }
